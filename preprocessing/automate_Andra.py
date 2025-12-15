366b83455f50ff3baf2f1af09b2b72e14596cdd0
Andra Pratama <andrapratama050205@gmail.com>
2025-12-15 18:22:47 +0700

    Add: weather_dataset.csv and preprocessing/

---
+++ preprocessing/automate_Andra.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import os
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+
+def process_data(input_path, output_path):
+    if not os.path.exists(input_path):
+        raise FileNotFoundError(f'File tidak ditemukan pada lokasi {input_path}')
+    
+    df = pd.read_csv(input_path)
+    
+    # Menangani nilai kosong dan Membersihkan data duplikat
+    df = df.dropna()
+    df = df.drop_duplicates()
+    
+    # Label encoding
+    le = LabelEncoder()
+    categorical_features = df.select_dtypes(include='object').columns
+    
+    for col in categorical_features:
+        df[col] = le.fit_transform(df[col])
+        
+    # Mengatasi Outlier
+    numeric_features = df.select_dtypes(include='number').columns
+    Q1 = df[numeric_features].quantile(0.25)
+    Q3 = df[numeric_features].quantile(0.75)
+    IQR = Q3 - Q1
+    
+    condition = ~((df[numeric_features] < (Q1 - 1.5 * IQR)) | (df[numeric_features] > (Q3 + 1.5 * IQR))).any(axis=1)
+    df_filtered_numeric = df.loc[condition, numeric_features] 
+
+    df = pd.concat([df_filtered_numeric, df.loc[condition, categorical_features]], axis=1)
+    
+    # Normalization
+    scaler = StandardScaler()
+    df[numeric_features] = scaler.fit_transform(df[numeric_features])   
+    
+    # Rename to target
+    df = df.rename(columns={'Weather Type': 'target'})
+    
+    
+    # Simpan Data
+    df.to_csv(output_path, index=False)
+    
+
+def main():
+    INPUT_FILE = os.path.join('../weather_dataset.csv')
+    OUTPUT_FILE = os.path.join('weather_dataset_preprocessing.csv')
+    
+    try:
+        process_data(INPUT_FILE, OUTPUT_FILE)
+    except Exception as e:
+        print(f"Terjadi kesalahan: {e}")
+
+if __name__ == '__main__':
+    main()
